d24c7deadbdde693636b539c1918a72801c10759
Daniel Wozniak <daniel.wozniak@traxtech.com>
2017-01-04 23:52:57 -0700

    Add 32bit elf section header struct

--- src/firmware/multiboot.c
+++ src/firmware/multiboot.c
@@ -121,6 +121,19 @@ struct elf_ehdr {
     uint16_t e_shstrndx;
 };
 
+struct elf32_shdr {
+    uint32_t    sh_name;
+    uint32_t    sh_type;
+    uint32_t    sh_flags;
+    uint32_t    sh_addr;
+    uint32_t    sh_offset;
+    uint32_t    sh_size;
+    uint32_t    sh_link;
+    uint32_t    sh_info;
+    uint32_t    sh_addralign;
+    uint32_t    sh_entsize;
+};
+
 #define EM_X86_64 62
 
 struct elf_phdr {
